58e57acd5257309f700689d8ecf1bf64956a4a60
nick-hue <nickagelid@gmail.com>
2023-12-11 17:36:37 +0200

    test works, not input data

--- day3/main.py
+++ day3/main.py
@@ -5,15 +5,11 @@ with open("input.txt") as f:
     #print(data)
 
 def isPart(character):
-    #print(f"Character: {character}")
     if character == "." or character.isdigit():
-        #print("Not a part.")
         return False
-    #print("Is part")
     return True
 
 def is_adjacent_to_part(data, line_number, index):
-    
     for i in range(line_number-1, line_number+2):
         for j in range(index-1, index+2):
             try:
@@ -32,19 +28,20 @@ for line_number, line in enumerate(data):
             numbers.append(number)
 print(numbers)
 
-part_numbers = []
-for number in numbers:
+number_set = [(num, False) for num in numbers]
+print(number_set)
+results = []
+for number, counted in number_set:
     for line_number, line in enumerate(data):
         if str(number) in line:
-            #print(f"{number} exists in line : {line_number}") 
             for digit in str(number):
                 index = line.find(digit)
-                if is_adjacent_to_part(data, line_number, index):
-                    part_numbers.append(number)
+                if is_adjacent_to_part(data, line_number, index) and not counted:
+                    counted = True
+                    results.append(number)
                     break
     
-print(part_numbers)
+print(results)
 # mporei na metrisei 2 fores to idio noumero
-
-total = sum(part_numbers)
+total = sum(results)
 print(total)
