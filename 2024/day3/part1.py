798d7d59f7b05123ed7dcb890160d292aeffc272
nick-hue <nickagelid@gmail.com>
2024-12-04 23:24:03 +0200

    not ended part1 day3

--- 2024/day3/part1.py
+++ 2024/day3/part1.py
@@ -5,10 +5,49 @@ class App():
         
         return data
 
-
     def solve(self):
-        input_data = self.read_from_file()
-        print(f"{input_data=}")       
+        input_data = self.read_from_file("input.txt")
+        # print(f"{input_data=}")       
+
+        total = 0
+        for line in input_data:
+            split_up = line.split("mul")
+            # print(f"{split_up=}")
+            for compute in split_up:
+                # print(f"{compute=}")
+                num_string = ''
+                num1 = 0
+                num2 = 0
+                possible_mul = False
+                for char in compute:
+                    if char == '(' and not possible_mul:
+                        # print("open par")
+                        possible_mul = True
+                    elif char == ')' and possible_mul:
+                        # print("close par")
+                        # print(f"{num_string=}")
+                        if num_string:
+                            num2 = int(num_string)
+                    elif char == ',' and possible_mul:
+                        # print(f"{num_string=}")
+                        if num_string:
+                            num1 = int(num_string)
+                        num_string = ''
+                    elif char.isdigit() and possible_mul:
+                        # print("put digit")
+                        num_string += char
+                    else:
+                        possible_mul = False
+                        num1 = num2 = 0
+                        num_string = ''
+                    print(char, end = '')
+                if num1 != 0 and num2 != 0:
+                    print(f"\n{num1=}, {num2=}")
+                total += num1 * num2
+
+        print(f"{total=}")
+                    
+            
 
 
 if __name__ == "__main__":
