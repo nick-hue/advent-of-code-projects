1d87d3c4a2512dca6b66817672507418ef60214b
nick-hue <nickagelid@gmail.com>
2024-12-07 18:21:27 +0200

    started day4

---
+++ 2024/day4/part2.py
@@ -0,0 +1,14 @@
+class App():
+    def read_from_file(self, filename="input.txt"):
+        with open(filename, "r") as f:
+            data = [line.strip() for line in f.readlines()]
+        return data
+
+    def solve(self):
+        input_data = self.read_from_file()
+        #input_data = self.read_from_file("input.txt")
+        print(f"{input_data=}")       
+
+if __name__ == "__main__":
+    App().solve()
+
