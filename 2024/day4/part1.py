87dba1425e6a4a256c68a0d2bda008e04d76eb77
nick-hue <nickagelid@gmail.com>
2024-12-07 18:45:07 +0200

    updated scripts

--- 2024/day4/part1.py
+++ 2024/day4/part1.py
@@ -1,13 +1,35 @@
 class App():
+    def __init__(self):
+        self.result_matrix = []
+        self.input_data = []
+
+
     def read_from_file(self, filename="input.txt"):
         with open(filename, "r") as f:
             data = [line.strip() for line in f.readlines()]
-        return data
+        self.input_data = data
+
+    def check_horizontal(self, input_data, x, y):
+        if input_data:
+            ...
+
+
 
     def solve(self):
-        input_data = self.read_from_file()
-        #input_data = self.read_from_file("input.txt")
-        print(f"{input_data=}")       
+        self.read_from_file("input_small.txt")
+        #self.read_from_file()
+        print(f"{self.input_data=}")    
+        
+        result_matrix = [False for line in self.input_data for character in line]
+
+        print(result_matrix)
+
+        for i, line in enumerate(self.input_data):
+            for j, character in enumerate(line):
+                ...
+                
+
+
 
 if __name__ == "__main__":
     App().solve()
