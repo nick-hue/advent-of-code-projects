405756abe3c86c24b1989b3e976e6ea8bba1ac8f
nick-hue <nickagelid@gmail.com>
2024-12-07 18:20:52 +0200

    part 2 ended

--- 2024/day3/part2.py
+++ 2024/day3/part2.py
@@ -1,3 +1,5 @@
+import re
+
 class App():
     def read_from_file(self, filename="input.txt"):
         with open(filename, "r") as f:
@@ -7,8 +9,38 @@ class App():
 
 
     def solve(self):
-        input_data = self.read_from_file()
+        input_data = self.read_from_file("input.txt")
         print(f"{input_data=}")       
+
+        total = 0
+        computing = True
+        data = "".join(input_data)
+
+        while data != "":
+            if computing:
+                split_result = re.split(r"don't\(\)", data, 1)
+            else:
+                split_result = re.split(r"do\(\)", data, 1)
+
+            print(split_result)
+            if len(split_result) > 1:
+                compute_part, remain_part = split_result
+            else:
+                compute_part, remain_part = split_result[0], "" 
+
+            if computing:
+                matches = re.findall(r"mul\(\d+,\d+\)", compute_part)
+                for compute in matches:
+                    first, second = compute.split(",")
+                    total += int(first.replace("mul(", "")) * int(second.replace(")", ""))
+                computing = False
+            else:
+                computing = True
+
+            data = remain_part
+
+        print(f"Total is : {total}")
+
 
 
 if __name__ == "__main__":
