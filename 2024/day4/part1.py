57824f1b05737d82961cf1fb8b38f591bca27757
nick-hue <nickagelid@gmail.com>
2024-12-07 20:48:57 +0200

    got result matrix to work

--- 2024/day4/part1.py
+++ 2024/day4/part1.py
@@ -19,17 +19,30 @@ class App():
         for i, line in enumerate(self.input_data):
             for j, char in enumerate(line):
                 # print(i,j)
-                string = "".join(self.input_data[i][j:j+4])
-                # print(string)
+                string = ""
+                try:
+                    list_of_chars = self.input_data[i][j] + self.input_data[i][j+1] + self.input_data[i][j+2] + self.input_data[i][j+3] 
+                except IndexError:
+                    break
 
+                string = "".join("".join(list_of_chars))
+                # print(string)
                 if string == "XMAS" or string == "SAMX":
                     total+=1
+                    print(f"Found {i}-{j} : {string}")
+                    self.result_matrix[i][j] = True
+                    self.result_matrix[i][j+1] = True
+                    self.result_matrix[i][j+2] = True
+                    self.result_matrix[i][j+3] = True
+            
+                    
         return total 
     
     def check_vertical(self):
         total = 0 
-
+        print(f"{len(self.input_data[0])=}")
         for i in range(len(self.input_data)):
+            string = ""
             for j in range(len(self.input_data[0])):
 
                 try:
@@ -38,10 +51,14 @@ class App():
                     break
                     
                 string = "".join(list_of_chars)
-                print(string)
                 if string == "XMAS" or string == "SAMX":
                     total+=1
-                
+                    print(f"Found {i}-{j} : {string}")
+                    self.result_matrix[i][j] = True
+                    self.result_matrix[i+1][j] = True
+                    self.result_matrix[i+2][j] = True
+                    self.result_matrix[i+3][j] = True
+
         return total
     
     def check_diagonally(self):
@@ -51,8 +68,9 @@ class App():
             for j in range(len(self.input_data[0])):
                 
                 try:
-                    left_diag_char = self.input_data[i][j] + self.input_data[i+1][j+1] + self.input_data[i+2][j+2] + self.input_data[i+3][j+3] 
-                    right_diag_char = self.input_data[i][j] + self.input_data[i-1][j-1] + self.input_data[i-2][j-2] + self.input_data[i-3][j-3] 
+                    left_diag_char = self.input_data[i][j] + self.input_data[i-1][j-1] + self.input_data[i-2][j-2] + self.input_data[i-3][j-3] 
+                    right_diag_char = self.input_data[i][j] + self.input_data[i+1][j+1] + self.input_data[i+2][j+2] + self.input_data[i+3][j+3] 
+                    
                 except IndexError:
                     break 
 
@@ -61,14 +79,27 @@ class App():
                 
                 if string_left == "XMAS" or string_left == "SAMX":
                     total+=1
+                    print(f"Found {i}-{j} : {string_left}")
+                    self.result_matrix[i][j] = True
+                    self.result_matrix[i-1][j-1] = True
+                    self.result_matrix[i-2][j-2] = True
+                    self.result_matrix[i-3][j-3] = True
+
                 if string_right == "XMAS" or string_right == "SAMX":
                     total+=1
-                
+                    print(f"Found {i}-{j} : {string_right}")
+                    self.result_matrix[i][j] = True
+                    self.result_matrix[i+1][j+1] = True
+                    self.result_matrix[i+2][j+2] = True
+                    self.result_matrix[i+3][j+3] = True
+                    
+
         return total
                 
     def draw_result_matrix(self):
         for i, line in enumerate(self.result_matrix):
             for j, value in enumerate(line):
+                # print(value)
                 if value:
                     print_char = self.input_data[i][j]
                 else:
@@ -82,17 +113,25 @@ class App():
         #self.read_from_file()
 
 
-        # self.result_matrix = [[False]*len(self.input_data[0])]*len(self.input_data)
+        self.result_matrix = [
+            [False for _ in range(len(self.input_data[0]))]
+            for _ in range(len(self.input_data))
+        ]
         # self.input_data = [list(line) for line in self.input_data]
         # self.input_data = np.array(self.input_data, dtype=str)
 
         result_total = 0
     
+        print("Checking horizontally")
         result_total += self.check_horizontal()
+        print("Checking vertically")
         result_total += self.check_vertical()
+        print("Checking diagonally")
         result_total += self.check_diagonally()
 
         print(f"{result_total=}")
+        # print(self.result_matrix)
+        self.draw_result_matrix()
 
 
 if __name__ == "__main__":
