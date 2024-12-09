ee5d40edd510ecf8070efaf73283f19081c09954
nick-hue <nickagelid@gmail.com>
2024-12-09 15:57:13 +0200

    done day4 part 1

--- 2024/day4/part1.py
+++ 2024/day4/part1.py
@@ -29,7 +29,7 @@ class App():
                 # print(string)
                 if string == "XMAS" or string == "SAMX":
                     total+=1
-                    print(f"Found {i}-{j} : {string}")
+                    # print(f"Found {i}-{j} : {string}")
                     self.result_matrix[i][j] = True
                     self.result_matrix[i][j+1] = True
                     self.result_matrix[i][j+2] = True
@@ -53,7 +53,7 @@ class App():
                 string = "".join(list_of_chars)
                 if string == "XMAS" or string == "SAMX":
                     total+=1
-                    print(f"Found {i}-{j} : {string}")
+                    # print(f"Found {i}-{j} : {string}")
                     self.result_matrix[i][j] = True
                     self.result_matrix[i+1][j] = True
                     self.result_matrix[i+2][j] = True
@@ -61,43 +61,56 @@ class App():
 
         return total
     
+    def check_left_diag(self, i, j):
+        string_left = ""
+        if j-3<0:
+            return 0
+        try:
+            string_left = self.input_data[i][j] + self.input_data[i+1][j-1] + self.input_data[i+2][j-2] + self.input_data[i+3][j-3] 
+            if string_left == "XMAS" or string_left == "SAMX":
+                # print(f"Found {i}-{j} : {string_left}")
+                self.result_matrix[i][j] = True
+                self.result_matrix[i+1][j-1] = True
+                self.result_matrix[i+2][j-2] = True
+                self.result_matrix[i+3][j-3] = True
+                return 1
+        except IndexError:
+            string_left = ''
+
+        # print(f"Left diag At {i}-{j} : {string_left}")
+
+        return 0
+
+    def check_right_diag(self, i, j):
+        string_right = ""
+        try:
+            string_right = self.input_data[i][j] + self.input_data[i+1][j+1] + self.input_data[i+2][j+2] + self.input_data[i+3][j+3] 
+            if string_right == "XMAS" or string_right == "SAMX":
+                # print(f"Found {i}-{j} : {string_right}")
+                self.result_matrix[i][j] = True
+                self.result_matrix[i+1][j+1] = True
+                self.result_matrix[i+2][j+2] = True
+                self.result_matrix[i+3][j+3] = True
+                return 1
+        except IndexError:
+            string_right = ''
+        # print(f"Right diag At {i}-{j} : {string_right}")
+        return 0
+
     def check_diagonally(self):
         total = 0 
 
         for i in range(len(self.input_data)):
             for j in range(len(self.input_data[0])):
-                
-                try:
-                    left_diag_char = self.input_data[i][j] + self.input_data[i-1][j-1] + self.input_data[i-2][j-2] + self.input_data[i-3][j-3] 
-                    right_diag_char = self.input_data[i][j] + self.input_data[i+1][j+1] + self.input_data[i+2][j+2] + self.input_data[i+3][j+3] 
-                    
-                except IndexError:
-                    break 
-
-                string_left = "".join(left_diag_char)
-                string_right = "".join(right_diag_char)
-                
-                if string_left == "XMAS" or string_left == "SAMX":
-                    total+=1
-                    print(f"Found {i}-{j} : {string_left}")
-                    self.result_matrix[i][j] = True
-                    self.result_matrix[i-1][j-1] = True
-                    self.result_matrix[i-2][j-2] = True
-                    self.result_matrix[i-3][j-3] = True
-
-                if string_right == "XMAS" or string_right == "SAMX":
-                    total+=1
-                    print(f"Found {i}-{j} : {string_right}")
-                    self.result_matrix[i][j] = True
-                    self.result_matrix[i+1][j+1] = True
-                    self.result_matrix[i+2][j+2] = True
-                    self.result_matrix[i+3][j+3] = True
-                    
+                total += self.check_left_diag(i, j)
+                total += self.check_right_diag(i, j)
 
         return total
                 
     def draw_result_matrix(self):
+        print("     0123456789")
         for i, line in enumerate(self.result_matrix):
+            print(f"{i} : ", end = " ")
             for j, value in enumerate(line):
                 # print(value)
                 if value:
@@ -105,13 +118,12 @@ class App():
                 else:
                     print_char = "."
 
-                print(print_char, end = '') 
+                print(f"{print_char}", end = '') 
             print()   
 
     def solve(self):
-        self.read_from_file("input_small.txt")
-        #self.read_from_file()
-
+        # self.read_from_file("input_small.txt")
+        self.read_from_file()
 
         self.result_matrix = [
             [False for _ in range(len(self.input_data[0]))]
@@ -131,7 +143,7 @@ class App():
 
         print(f"{result_total=}")
         # print(self.result_matrix)
-        self.draw_result_matrix()
+        # self.draw_result_matrix()
 
 
 if __name__ == "__main__":
