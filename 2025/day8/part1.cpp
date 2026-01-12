fd897239a08301e7b2a4ad2474d9cf8a2449fee1
nick-hue <nickagelid@gmail.com>
2026-01-12 20:04:22 +0200

    did not start

--- 2025/day8/part1.cpp
+++ 2025/day8/part1.cpp
@@ -1,13 +1,26 @@
 
 #include "../../util.hpp"
 
+struct Vector3 {
+    int x, y, z;
+} vector3 ;
+
+void printv(Vector3 v){
+    printf("%d-%d-%d\n", v.x, v.y, v.z);
+}
+
 int main() {
     auto lines = readInput("input_small.txt");
     //auto lines = readInput("input.txt");   
 
-    for (auto& line : lines){
-        printf("%s\n", line.c_str());
+    vector<Vector3> nums;
+    transform(lines.begin(), lines.end(), back_inserter(nums), [](string& str) { auto split = splitString(str, ','); return Vector3{stoi(split[0]), stoi(split[1]), stoi(split[3]) });
+    for (int i = 0; i < nums.size(); i++){
+            
     }
+
+
+    
 
     return 0;
 }
